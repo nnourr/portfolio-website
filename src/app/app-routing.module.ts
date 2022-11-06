0ddbd988d2f9c5c137e6369f070dc06c1ad15b9b
nnourr <nourqwerty123@gmail.com>
2022-11-05 22:27:28 -0400

    new pfp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
 
 const routes: Routes = [
   {path: "", component: HomePageComponent}
--- src/app/components/project-page/project-page.component.ts
+++ src/app/components/project-page/project-page.component.ts
@@ -9,7 +9,13 @@ import { animate, animateChild, group, query, style, transition, trigger } from 
     trigger('projectPageAnimation', [
       transition(':enter', [
         style({width:'100%', height:'100%', maxWidth:'100%', borderRadius:0, boxShadow:"0 0 0 black"}),
-        animate("1000ms 1700ms", style({width:"100%", height:"80%", maxWidth:'900px', borderRadius:"1rem"})),
+        animate("1000ms 1700ms", style({width:"100%", height:"90vh", maxWidth:'900px', borderRadius:"1rem"})),
+      ])
+    ]),
+    trigger('textAnimation', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate("1000ms 3400ms", style({opacity:1})),
       ])
     ])
   ]
