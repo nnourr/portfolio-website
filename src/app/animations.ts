cf2917dd2fbf5d17c16e3f0179b5cb8bdb8cf8f2
nnourr <nourqwerty123@gmail.com>
2023-08-31 09:59:58 -0400

    some updates

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -4,15 +4,17 @@ export const slideInAnimation =
   trigger('routeAnimations', [
     transition('* <=> *', [
     query(':enter', [
-        style({ opacity: '100%' })
+        style({ opacity: 0}),
+        animate('300ms ease-in', style({ opacity: 1 }))
     ], { optional: true }),
-    query(':leave', [
-        style({ opacity: '0' })
-    ], { optional: true }),
+    // query(':leave', [
+    //     style({ opacity: '1' }),
+    //     animate('300ms ease-out', style({ transform: 'translateX(100%)', opacity: 0 }))
+    // ]),
     //   query(':leave', animateChild(), { optional: true }),
     //   group([
     //     query(':leave', [
-    //       animate('300ms ease-out', style({ left: '100%', opacity: 0 }))
+    //       animate('300ms ease-out', style({ left: '100%' }))
     //     ], { optional: true }),
     //     query(':enter', [
     //       animate('300ms ease-out', style({ left: '0%' }))
