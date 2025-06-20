2de39cac179d7877e4848a923e27bccd86f21ada
nnourr <nourqwerty123@gmail.com>
2025-06-20 00:31:48 -0400

    clean up home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
         extend: {
             colors: {
                 'accent': 'rgb(227, 45, 145)',
-                // 'accent': 'rgb(227, 45, 145)',
                 'dark-background': 'rgb(22, 22, 22)',
                 'light-background': '#f5f5f5',
             },
@@ -17,9 +16,13 @@ module.exports = {
             boxShadow: {
               'accent-solid': 'inset 0 -2px 0',
             },
-    fontFamily: {
-        'roboto': ['Roboto Mono', 'sans-serif'],
-      }
+            fontFamily: {
+                'roboto': ['Roboto Mono', 'sans-serif'],
+                'courier': ['Courier New', 'Courier', 'monospace'],
+            },
+            fontSize: {
+                'base': '24px',
+            }
         }
     },
     plugins: [],
